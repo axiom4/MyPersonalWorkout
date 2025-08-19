12f172e3431463248381c4632657c8a23f6319a7
Riccardo GIannetto <rgiannetto@gmail.com>
2025-08-19 16:18:51 +0200

    Add Initial page

--- MyPersonalWorkout Watch App/ContentView.swift
+++ MyPersonalWorkout Watch App/ContentView.swift
@@ -8,17 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+  var body: some View {
+    ScrollView {
+      
+      VStack {
+        Text("My Personal Workout")
+          .font(.callout)
+          .foregroundColor(.primary)
+        
+        Image("Icon")
+          .resizable()
+          .scaledToFit()
+          .frame(width: 100, height: 100)
+          .foregroundStyle(.primary)
+          .padding()
+        
+        Text("This is a watchOS app for tracking workouts.")
+          .font(.subheadline)
+          .foregroundColor(.secondary)
+          .padding(.bottom, 10)
+        
+        
+        Button("Start Workout") {
+          // Azione da eseguire quando il pulsante viene premuto
+          print("Workout started!")
         }
         .padding()
+      }
     }
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }
